eca0b6df86e2ec77b47495a01c969f1cfb757aad
Henning Spjelkavik <henning@spjelkavik.net>
2014-05-14 00:16:41 +0200

    Working splits.

--- web/nh/nh2.php
+++ web/nh/nh2.php
@@ -22,12 +22,22 @@ $m = new Mustache_Engine(array(
         return htmlspecialchars($value, ENT_COMPAT, 'ISO-8859-1');},
 ));
 
+$splits = array(
+    "NightHawk Men"=>array(5=>array("1121","1111","1141","1136")),
+    "NightHawk Women"=>array(4=>array("1136"))
+);
+
+
 $app = new \Slim\Slim(array('debug'=>true));
 
-$app->get('/:raceId/:class/result/:leg', function ($raceId,$class,$leg) {
-    global $m;
+$app->get('/:raceId/:class/result/:leg/:split', function ($raceId,$class,$leg,$split) {
+    global $m, $splits;
     $comp = new Emma($raceId);
-    $results = result_by_leg($class, $comp, $leg);
+    if ($split>0) {
+        $results = result_by_leg_at_split($class, $comp, $leg, $splits[$class][$leg][$split-1]);
+    } else {
+        $results = result_by_leg($class, $comp, $leg);
+    }
     echo $m->render("total", array("res"=>$results, "class"=>$class));	
 });
 
@@ -53,7 +63,7 @@ $app->run();
 
 function result_by_leg ($class, $comp, $leg) {
 
-    $q = "SELECT relay_teamid,Runners.Club, max(Results.Status) as Status, sum(relay_restarts) as Restarts, sum(relay_leg_time) as Time, count(Runners.DbId) as NumFinish From Runners,Results where Results.DbID = Runners.DbId AND Results.TavId = ". $comp->m_CompId ." AND Runners.TavId = ".$comp->m_CompId ." AND Runners.Class like '".$class."-_' AND Control=1000 AND relay_leg<=".$leg." GROUP BY relay_teamid,Club ORDER BY status,NumFinish desc,restarts, time";
+    $q = "SELECT * FROM (select relay_teamid, Runners.Club, max(Results.Status) as Status, sum(relay_restarts) as Restarts, sum(relay_leg_time) as Time, count(Runners.DbId) as NumFinish From Runners,Results where Results.DbID = Runners.DbId AND Results.TavId = ". $comp->m_CompId ." AND Runners.TavId = ".$comp->m_CompId ." AND Runners.Class like '".$class."-_' AND Control=1000 AND relay_leg<=".$leg." GROUP BY relay_teamid,Club ORDER BY status,NumFinish desc,restarts, time) as total, (select relay_teamid, Name, relay_leg_time as LegTime, re.Status as LegStatus from Results re, Runners ru WHERE re.dbid=ru.dbid and re.tavid=".$comp->m_CompId." and ru.tavid=".$comp->m_CompId." and relay_leg=".$leg." and class like '".$class."-_' AND control=1000)  as leg_runner WHERE total.relay_teamid=leg_runner.relay_teamid ORDER by status, NumFinish desc, restarts, time";
 
     if ($result = mysql_query($q,$comp->m_Conn)) {
         $results = array();
@@ -80,13 +90,18 @@ function result_by_leg ($class, $comp, $leg) {
                 $prevrestart = $row["Restarts"];
                 if ($n>1)$restart_flag= $row["Restarts"]-5;
             }
+
+	    if ($row["Status"]>0) { $rank=""; }
+
             array_push($results, array(
                 "Club"=>$row["Club"], "Status"=>$row["Status"],
                 "Timestr"=>formatTime($row["Time"], $row["Status"]),
                 "Restarts"=>$row["Restarts"],
                 "TeamId"=>$row["relay_teamid"], "Rank"=>$rank,
                 "RestartFlag"=>$restart_flag, "NumFinish"=>$row["NumFinish"],
-                "Behind" => ($delta>0) ? "+".formatTimeSimple($delta,0):""
+                "Behind" => ($delta>0) ? "+".formatTimeSimple($delta,0):"",
+		"LegStatus"=>$row["Status"],
+		"Name" => $row["Name"], "LegTime"=>formatTime($row["LegTime"],0)." ".status($row["LegStatus"])
             )
             );
         }
@@ -103,6 +118,82 @@ function result_by_leg ($class, $comp, $leg) {
 
 }
 
+function result_by_leg_at_split ($class, $comp, $leg, $split) {
+
+# sum(relay_leg_time) leg<$leg + Time at current to control
+
+    $q = "
+SELECT total.relay_teamid, Club, Status, Restarts, total.Time+leg_runner.SplitTime as Time, 
+       NumFinish, 
+       LegStatus, Name, SplitTime
+FROM
+ (select relay_teamid, Runners.Club, max(Results.Status) as Status, 
+  sum(relay_restarts) as Restarts, sum(relay_leg_time) as Time, count(Runners.DbId) as NumFinish
+  FROM Runners,Results where Results.DbID = Runners.DbId 
+    and Results.TavId = ". $comp->m_CompId ."
+    and Runners.TavId = ".$comp->m_CompId ." AND Runners.Class like '".$class."-_' 
+    and Control=1000 AND relay_leg<".$leg." 
+  GROUP BY relay_teamid,Club 
+  ORDER BY status,NumFinish desc,restarts, time) as total, 
+  (SELECT relay_teamid, Name, relay_leg_time as SplitTime, re.Status as LegStatus 
+   FROM Results re, Runners ru 
+   WHERE re.dbid=ru.dbid and re.tavid=".$comp->m_CompId." and ru.tavid=".$comp->m_CompId." 
+    and relay_leg=".$leg." and class like '".$class."-_' AND control=".$split.") as leg_runner
+WHERE total.relay_teamid=leg_runner.relay_teamid 
+ORDER by status, NumFinish desc, restarts, time
+";
+    if ($result = mysql_query($q,$comp->m_Conn)) {
+        $results = array();
+        $rank = 1;
+        $besttime = 0;
+        $prevtime = 0;
+        $prevrestart = 0;
+        $n = 0;	
+        
+        while ($row = mysql_fetch_array($result)) {
+            $n++;
+            if ($row["Time"]!=$prevtime) {
+                $rank = $n;
+                $prevtime = $row["Time"];
+            }
+        	$restart_flag=0;
+            $delta = 0;
+            if ($n==1) { $besttime = $row["Time"]; }
+            else { 
+                $delta = $row["Time"] - $besttime; 
+            }
+            
+            if ($prevrestart<$row["Restarts"]) {
+                $prevrestart = $row["Restarts"];
+                if ($n>1)$restart_flag= $row["Restarts"]-5;
+            }
+
+	    if ($row["Status"]>0) { $rank=""; }
+
+            array_push($results, array(
+                "Club"=>$row["Club"], "Status"=>$row["Status"],
+                "Timestr"=>formatTime($row["Time"], $row["Status"]),
+                "Restarts"=>$row["Restarts"],
+                "TeamId"=>$row["relay_teamid"], "Rank"=>$rank,
+                "RestartFlag"=>$restart_flag, "NumFinish"=>$row["NumFinish"],
+                "Behind" => ($delta>0) ? "+".formatTimeSimple($delta,0):"",
+		"LegStatus"=>$row["Status"],
+		"Name" => $row["Name"], "LegTime"=>formatTime($row["SplitTime"],0)." ".status($row["LegStatus"])
+            )
+            );
+        }
+        mysql_free_result($result);
+        return $results;
+        
+    }		else {
+        
+        die(mysql_error());
+    }
+    
+
+
+
+}
 
 
 function list_all_result ($class, $comp) {
@@ -130,6 +221,14 @@ function list_all_result ($class, $comp) {
         die(mysql_error());
     }
  }
+
+function status($status){
+	 global $RunnerStatus;
+    if ($status != "0")  {
+      return $RunnerStatus[$status]; //$status;
+    }
+    return "";
+}
 
 function formatTime($time,$status){
     global $lang;
